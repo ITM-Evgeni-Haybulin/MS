12e397dc8573e9a0e553e3005e313d8f756f72d3
Freest1999 <zhenya.khaybulin@inbox.ru>
2023-07-13 20:44:19 +0500

    commit

--- backend-gateway-client/src/test/java/com/itm/space/backend/client/BackendGatewayClientApplicationTests.java
+++ backend-gateway-client/src/test/java/com/itm/space/backend/client/BackendGatewayClientApplicationTests.java
@@ -2,6 +2,7 @@ package com.itm.space.backend.client;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 @SpringBootTest
 class BackendGatewayClientApplicationTests {
